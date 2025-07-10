4d94015a0efe7d9c308ab53be8eeefd0442549d4
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-07-10 13:02:52 +0530

    Time: 6 ms (11.55%), Space: 44.9 MB (77.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -22,7 +22,7 @@ class Solution {
     public int tab(String s1,String s2,int dp[][]){
         int n1 = s1.length();
         int n2 = s2.length();
-        for(int i=1;i<=n1;i++)
+        for(int i=0;i<=n1;i++)
             dp[i][0] = i; 
         for(int j=0;j<=n2;j++)
             dp[0][j] = j;
