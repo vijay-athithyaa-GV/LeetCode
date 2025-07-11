f500170fd41e939d1f4dec2d395a564581c0c742
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-07-11 13:02:28 +0530

    Time: 28 ms (8.32%), Space: 49.4 MB (5.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -5,7 +5,7 @@ class Solution {
         int n1 = p.length();
         int n2 = s.length();
         Boolean dp[][] = new Boolean[n1+1][n2+1];
-        return mat(n1,n2,p,s,dp);
+        return tab(p,s,dp);
     }
     public boolean mat(int i,int j,String s1,String s2,Boolean dp[][]){
         //base case..
@@ -31,5 +31,36 @@ class Solution {
         dp[i][j] = false; 
         return dp[i][j];
 
+    }
+    public boolean tab(String s1,String s2,Boolean dp[][]){
+        int n1 = s1.length();
+        int n2 = s2.length();
+        dp[0][0] = true;
+        for(int j=1;j<=n2;j++)
+            dp[0][j] = false;
+        for(int i=1;i<=n1;i++){
+            boolean flg = true;
+            for(int k=1;k<=i;k++){
+                if(s1.charAt(k-1) != '*'){
+                    flg = false;
+                    break;
+                }
+            }
+            dp[i][0] = flg;
+        }
+
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1) || s1.charAt(i-1) =='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(s1.charAt(i-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else
+                    dp[i][j] = false; 
+            }     
+        }
+        return dp[n1][n2];
     }
 }
