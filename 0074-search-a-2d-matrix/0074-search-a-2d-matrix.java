f210023904bac6362a0ff8694a2ec5739fcbd202
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-07-31 17:13:20 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,18 +2,25 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
-        boolean flg = false;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j] == target){
-                    flg = true;
-                    break;
-                }
-            }
-            if(flg)
-                break;
-
+           if(matrix[i][0]<=target && target<=matrix[i][m-1]){
+                return binary(matrix[i],target);
+           }
+        } 
+        return false;
+    }
+    public boolean binary(int[] arr,int tar){
+        int l = 0;
+        int r = arr.length-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(arr[mid] == tar)
+                return true;
+            else if(tar<arr[mid])
+                r = mid-1;
+            else 
+                l = mid+1;
         }
-        return flg;
+        return false;
     }
 }
