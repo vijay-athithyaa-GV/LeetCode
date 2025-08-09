854a3259f7b72d90efd5263718eb94abd9d74da5
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-08-09 10:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (28.08%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,5 +1,8 @@
 class Solution {
     public int trap(int[] height) {
+        return optimal(height);
+    }
+    public int better(int[] height){
         int n = height.length;
         int prefix[] = new int[n];
         prefix[0] = height[0];
@@ -20,5 +23,31 @@ class Solution {
             }
         }
         return total;
+    }
+
+    public int optimal(int[] height){
+        int n = height.length;
+        int lmax = 0;
+        int rmax = 0;
+        int total = 0;
+        int l = 0;
+        int r = n-1;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(lmax>height[l])
+                    total+=lmax - height[l];
+                else
+                    lmax = height[l];
+                l++;
+            }
+            else{
+                if(rmax>height[r])
+                    total+=rmax - height[r];
+                else
+                    rmax = height[r];
+                r--;
+            }
+        }
+        return  total;
     }
 }
