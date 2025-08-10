39b86f8f6bfec2ae02a5747d88532a15b3dc880f
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-08-11 00:09:48 +0530

    Time: 1 ms (30.01%) | Memory: 44.7 MB (13.41%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -25,8 +25,6 @@ class Solution {
         }
 
         while(count>=k){
-            if(count<k)
-                break;
             curr = prev.next;
             nex = curr.next;
             for(int i=1;i<k;i++){
