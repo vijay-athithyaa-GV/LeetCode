a20b16a4c8dad82e487403ebb3d477403a78a53c
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-09-19 19:52:43 +0530

    Time: 6 ms (97.05%) | Memory: 56.8 MB (96.11%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,8 +1,46 @@
 import java.util.*;
 
 class Solution {
+    Random rand = new Random();
+
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        int n = nums.length;
+        k = n - k;  // kth largest -> kth smallest
+        return quickSelect(nums, 0, n - 1, k);
+    }
+
+    private int quickSelect(int[] nums, int l, int r, int k) {
+        if (l == r) return nums[l];
+
+        // randomize pivot
+        int pivotIndex = l + rand.nextInt(r - l + 1);
+        int pivot = nums[pivotIndex];
+
+        // 3-way partition
+        int lt = l, i = l, gt = r;
+        while (i <= gt) {
+            if (nums[i] < pivot) {
+                swap(nums, lt, i);
+                lt++;
+                i++;
+            } else if (nums[i] > pivot) {
+                swap(nums, i, gt);
+                gt--;
+            } else {
+                i++;
+            }
+        }
+
+        // now: [l..lt-1] < pivot, [lt..gt] == pivot, [gt+1..r] > pivot
+
+        if (k >= lt && k <= gt) return nums[k];
+        else if (k < lt) return quickSelect(nums, l, lt - 1, k);
+        else return quickSelect(nums, gt + 1, r, k);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
