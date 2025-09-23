1fcf3737151b8bf7ea0988675d2d37105bf4b156
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-09-23 19:42:40 +0530

    Time: 3 ms (48.19%) | Memory: 42 MB (44.20%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> s1 = new Stack<>();
+        if(s.length()<=1)
+            return false;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='{'||c=='['){
+                s1.push(c);
+            }
+            else{
+                if(s1.empty())
+                    return false;
+                char top = s1.peek();
+                if(c==')' && top=='(')
+                        s1.pop();
+                else if(c==']' && top=='[')
+                        s1.pop();
+                else if(c=='}' && top=='{')
+                        s1.pop();
+                else
+                    s1.push(c);
+            }
+        }
+        if(!s1.empty())
+                return false;
+    return true;
+    }    
+}
