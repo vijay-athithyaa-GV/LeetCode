511a454e10ca86f3fdaa5784d9162ced10d790db
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2025-10-01 12:18:53 +0530

    Time: 118 ms (9.32%) | Memory: 57.7 MB (36.26%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,34 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        
-        // Nearest smaller to left
+        int max=0;
         Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
-            left[i] = st.isEmpty() ? -1 : st.peek();
+            left[i] = st.isEmpty()?-1:st.peek();
             st.push(i);
         }
-        
-        // Nearest smaller to right
         st.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+       
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
-            right[i] = st.isEmpty() ? n : st.peek();
+            right[i] = st.isEmpty()?n:st.peek();
             st.push(i);
         }
-        
-        // Calculate max area
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            int width = right[i] - left[i] - 1;
-            max = Math.max(max, heights[i] * width);
+        for(int i=0;i<n;i++){
+            int width = right[i]-left[i]-1;
+            max = Math.max(max,heights[i]*width);
         }
         return max;
     }
