610fecb1b035cc4f85a4035a743181497fff6b75
Vijay athithyaa g v <g.v.vijayathithya@gmail.com>
2026-01-02 22:57:50 +0530

    Time: 2 ms (81.82%) | Memory: 42.9 MB (65.29%) - LeetSync

---
+++ 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] numMovesStones(int a, int b, int c) {
+        int arr[] = {a,b,c};
+        Arrays.sort(arr);   
+        if(arr[2] - arr[0] == 2) 
+            return new int[]{0,0};
+        return new int[]{Math.min(arr[1]-arr[0],arr[2]-arr[1])<=2?1:2, arr[2]-arr[0]-2}; 
+    }
+}
