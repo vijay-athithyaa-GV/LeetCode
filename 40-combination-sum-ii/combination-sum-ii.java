3e5eba8b7c331e9823f1dd06ee2d617ecc4f767a
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-03-19 23:15:19 +0530

    Time: 6 ms (73.60%) | Memory: 45 MB (92.25%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
+        List<Integer> sub = new ArrayList<>();
         Arrays.sort(candidates);
-        comsum(0,target,new ArrayList<>(),res,candidates);
-        return res;
+        comb(0,candidates,sub,target,res);
+        List<List<Integer>> res1 = new ArrayList<>(res);
+        return res1;
     }
-    public void comsum(int ind,int tar,List<Integer> ds,List<List<Integer>> res,int[] can){
+
+    public void comb(int ind,int arr[],List<Integer> sub,int tar,Set<List<Integer>> res){
+        int n = arr.length;
         if(tar == 0){
-            res.add(new ArrayList<>(ds));
+            res.add(new ArrayList<>(sub));
             return;
-        } 
-        for(int i=ind;i<can.length;i++){
-            if(i>ind && can[i-1] == can[i]) continue;
-            if(tar<can[i]) break;
-            ds.add(can[i]);
-            comsum(i+1,tar-can[i],ds,res,can);
-            ds.remove(ds.size()-1);
+        }
+        for(int i=ind;i<n;i++){
+            if(i>ind && arr[i]==arr[i-1])
+                continue; 
+            if(arr[i]<=tar){
+                sub.add(arr[i]);
+                comb(i+1,arr,sub,tar-arr[i],res);
+                sub.remove(sub.size()-1);
+            }
         }
     }
 }
