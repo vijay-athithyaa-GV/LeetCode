8d0f1838fbc36af78a364466689ac8946fc857d8
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-06-11 23:15:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (76.79%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -3,22 +3,21 @@ import java.util.*;
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1)  return nums[0];
-        int dp1[] = new int[n];
-        int dp2[] = new int[n];
+        if(n == 1) return nums[0];
+        int dp1[] = new int[n+1];
+        int dp2[] = new int[n+1];
         Arrays.fill(dp1,-1);
         Arrays.fill(dp2,-1);
-        int lastleft = robdb(0,n-2,nums,dp1);
-        int fistleft = robdb(1,n-1,nums,dp2);
-        return Math.max(lastleft,fistleft);
+        return Math.max(robHouse(nums,1,n-1,dp1),robHouse(nums,0,n-2,dp2));
     }
-    public int robdb(int start,int ind,int arr[],int dp[]){
+
+    public int robHouse(int arr[],int start,int ind,int dp[]){
         if(ind == start) return arr[ind];
-        if(ind < start) return 0;
+        if(ind<start) return 0;
         if(dp[ind]!=-1) return dp[ind];
-        int pick = arr[ind]+robdb(start,ind-2,arr,dp);
-        int notpick = robdb(start,ind-1,arr,dp);
-        dp[ind] = Math.max(pick,notpick);
+        int take = arr[ind]+robHouse(arr,start,ind-2,dp);
+        int nottake = robHouse(arr,start,ind-1,dp);
+        dp[ind] = Math.max(take,nottake);
         return dp[ind];
     }
 }
