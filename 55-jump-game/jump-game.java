2a4ea070f7088fabe3b31644f4a120b88fe066ab
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-06-13 00:11:18 +0530

    Time: 1 ms (99.97%) | Memory: 47.9 MB (61.10%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,26 +1,12 @@
-import java.util.*;
-
 class Solution {
     public boolean canJump(int[] nums) {
-       int n = nums.length;
-       int dp[] = new int[n+1];
-       Arrays.fill(dp,-1);
-       return jump(nums,0,dp);
-    }
+        int n = nums.length;
+        int finalpos = n-1;
+        int ind = n-2;
 
-    public boolean jump(int arr[],int ind,int dp[]){
-        int n = arr.length;
-        if(ind == n-1) return true;
-        if(arr[ind] == 0 && (n-1)-ind>0) return false;
-        if(dp[ind]!=-1) return dp[ind]==1;
-        int size = arr[ind];
-        for(int i=ind+1;i<=size+ind;i++){
-            if(jump(arr,i,dp)){
-                dp[ind] = 1;
-                return true;
-            }
+        for(int i=ind;i>=0;i--){
+            if(i+nums[i]>=finalpos) finalpos = i;
         }
-        dp[ind] = 0;
-        return false;
+        return finalpos == 0;
     }
 }
