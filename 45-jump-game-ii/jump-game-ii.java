c12771014212d2ff8a08ce076f6e9eec5704c34a
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-06-16 00:16:36 +0530

    Time: 1 ms (99.71%) | Memory: 47 MB (92.38%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,24 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return canj(0,nums,dp);
-    }
-
-    public int canj(int ind,int nums[],int dp[]){
-        int n = nums.length;
-        if(ind == n-1) return 0;
-        if(nums[ind] == 0) return Integer.MAX_VALUE;
-        if(dp[ind]!=-1) return dp[ind];
-        int min = Integer.MAX_VALUE;
-        for(int i=ind+1;i<=Math.min(n-1,nums[ind]+ind);i++){
-            int take = canj(i,nums,dp);
-            if(take != Integer.MAX_VALUE)min = Math.min(min,1+take);  
+        int cov = 0;
+        int des = n-1,lastind = 0;
+        int totjump = 0;
+        if(nums.length == 1) return 0;
+        for(int i=0;i<n;i++){
+            cov = Math.max(cov,i+nums[i]);
+            if(i == lastind){
+                lastind = cov;
+                totjump++;
+                if(cov>=des){
+                    return totjump;
+                }
+            }
+            
         }
-        dp[ind] =  min;
-        return dp[ind];
+        return totjump;
     }
 }
