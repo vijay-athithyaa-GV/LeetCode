9213246f8d392c228d6a4622fe297851a5e38c36
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-06-27 00:09:04 +0530

    Time: 4 ms (45.50%) | Memory: 54.4 MB (10.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,45 +1,39 @@
 import java.util.*;
 
 class Solution {
+    int cnt = 0;
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int island=0;
+        
         int vis[][] = new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(vis[i],0);
+        }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == '1' && vis[i][j]!=1){
-                    island++;
-                    bfs(grid,n,m,i,j,vis);
+                    dfs(i,j,vis,grid);
+                    cnt++;
                 }
             }
         }
-        return island; 
+        return cnt;
     }
-    public void bfs(char[][] grid,int n,int m,int i,int j,int vis[][]){
-        Queue<int[]> q = new LinkedList<>();
-        vis[i][j] = 1;
-        q.add(new int[]{i,j});
-        while(!q.isEmpty()){
-            int[] pos = q.remove();
-            int x = pos[0];
-            int y = pos[1];
-            if((x+1)<n && grid[x+1][y] == '1' && vis[x+1][y]!=1){
-                q.add(new int[]{x+1,y});
-                vis[x+1][y]=1;
+
+    public void dfs(int row,int col,int vis[][],char grid[][]){
+        vis[row][col] = 1;
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int newr = row+dr[i];
+            int newc = col+dc[i];
+
+            if(newr>=0 && newr<grid.length && newc>=0 && newc<grid[0].length){
+                if(grid[newr][newc]=='1' && vis[newr][newc]!=1){
+                    dfs(newr,newc,vis,grid);
+                }
             }
-            if((y+1)<m && grid[x][y+1] == '1' && vis[x][y+1]!=1){
-                q.add(new int[]{x,y+1});
-                vis[x][y+1] = 1;
-            }
-            if((x-1)>=0 && grid[x-1][y] == '1' && vis[x-1][y]!=1){
-                q.add(new int[]{x-1,y});
-                vis[x-1][y] = 1;
-            }
-            if((y-1)>=0 && grid[x][y-1] == '1' && vis[x][y-1]!=1){
-                q.add(new int[]{x,y-1});
-                vis[x][y-1] = 1;
-            } 
         }
     }
 }
