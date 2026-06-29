9858d4b4fc4e005084ef45857d7dd477c0b621cb
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-06-29 23:55:58 +0530

    Time: 8 ms (91.24%) | Memory: 49.2 MB (41.61%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,13 +3,8 @@ import java.util.*;
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> res = new ArrayList<>();
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        // List<int[]> tem = new ArrayList<>();
-        // // for(int i=0;i<n;i++){
-        // //     int sub[]={intervals[i][0],intervals[i][1]};
-        // //     tem.add(sub);
-        // // }
         int prev[] = intervals[0];
         for(int i=1;i<n;i++){
             if(intervals[i][0]<=prev[1]){
@@ -23,6 +18,5 @@ class Solution {
         res.add(prev);
         int sol[][] = res.toArray(new int[res.size()][]);
         return sol;
-
     }
 }
