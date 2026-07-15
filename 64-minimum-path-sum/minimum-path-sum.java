a015e02293e9ea67729839f524f8e2967d4aaf30
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-07-16 00:00:13 +0530

    Time: 4 ms (62.90%) | Memory: 50.3 MB (55.98%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -6,7 +6,8 @@ class Solution {
         int m = grid[0].length;
         int dp[][] = new int[n][m];
         for(int i=0;i<n;i++) Arrays.fill(dp[i],Integer.MAX_VALUE);
-        return minPath(n-1,m-1,grid,dp);
+        //return minPath(n,m,grid,dp);
+        return tab(grid,dp);
     }
 
     public int minPath(int ind1,int ind2,int[][] grid,int[][] dp){
@@ -17,5 +18,30 @@ class Solution {
         if(ind2-1>=0) left = minPath(ind1,ind2-1,grid,dp)+grid[ind1][ind2];
         if(ind1-1>=0) top = minPath(ind1-1,ind2,grid,dp)+grid[ind1][ind2];
         return dp[ind1][ind2] = Math.min(left,top);
+    }
+
+    public int tab(int[][] grid,int[][] dp){
+        int n = grid.length;
+        int m = grid[0].length;
+        dp[0][0] = grid[0][0];
+        // for(int i=0;i<n;i++) dp[i][0] = 0;
+        // for(int i=0;i<m;i++) dp[0][i] = 0;
+
+        for(int i=1;i<n;i++){
+            dp[i][0] = grid[i][0]+dp[i-1][0];
+        }
+
+        for(int j=1;j<m;j++){
+            dp[0][j] = grid[0][j]+dp[0][j-1];
+        }
+
+        for(int ind1=1;ind1<n;ind1++){
+            for(int ind2=1;ind2<m;ind2++){
+                int left = dp[ind1][ind2-1];
+                int top =  dp[ind1-1][ind2];
+                dp[ind1][ind2] = Math.min(left,top)+grid[ind1][ind2];
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
