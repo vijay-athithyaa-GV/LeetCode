c7babb99c83d51cdf1494a783c24c5727eb34078
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-07-16 00:02:13 +0530

    Time: 1 ms (99.93%) | Memory: 50.4 MB (21.23%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -6,8 +6,8 @@ class Solution {
         int m = grid[0].length;
         int dp[][] = new int[n][m];
         for(int i=0;i<n;i++) Arrays.fill(dp[i],Integer.MAX_VALUE);
-        //return minPath(n,m,grid,dp);
-        return tab(grid,dp);
+        return minPath(n-1,m-1,grid,dp);
+        //return tab(grid,dp);
     }
 
     public int minPath(int ind1,int ind2,int[][] grid,int[][] dp){
