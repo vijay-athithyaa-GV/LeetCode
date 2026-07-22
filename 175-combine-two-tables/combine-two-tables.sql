c7ff906a079e1a5b9f42146e75e4de5b6ed76eea
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-07-22 13:28:55 +0530

    Time: 240 ms (83.00%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,4 @@
 -- Write your PostgreSQL query statement below
-select p.firstName as firstName,p.lastName as lastName,a.city as city,
-a.state as state from Person p left join Address a on p.personId = a.personId;
+select p.firstName,p.lastName,a.city,
+a.state
+from Person p left join Address a on p.personId = a.personId;
