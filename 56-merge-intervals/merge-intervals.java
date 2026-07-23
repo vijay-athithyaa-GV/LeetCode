0e9196f7fcbdce43f051d7d053869717738fb3dc
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-07-23 20:24:39 +0530

    Time: 9 ms (36.63%) | Memory: 49.1 MB (60.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,21 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<int[]> res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         int prev[] = intervals[0];
-        for(int i=1;i<n;i++){
-            if(intervals[i][0]<=prev[1]){
-                prev[1] = Math.max(prev[1],intervals[i][1]);
-            }
-            else{
-                res.add(prev);
+        
+        for(int i=1;i<intervals.length;i++){
+            int val[] = intervals[i];
+            if(val[0]<=prev[1]){
+                prev[1] = Math.max(prev[1],val[1]);
+            }else{
+                List<Integer> sub = new ArrayList<>();
+                sub.add(prev[0]);
+                sub.add(prev[1]);
+                res.add(sub);
                 prev = intervals[i];
             }
         }
-        res.add(prev);
-        int sol[][] = res.toArray(new int[res.size()][]);
-        return sol;
+        List<Integer> sub = new ArrayList<>();
+                sub.add(prev[0]);
+                sub.add(prev[1]);
+        res.add(sub);
+        int n = res.size();
+        int fin[][] = new int[n][2];
+        for(int i=0;i<n;i++){
+            fin[i][0] = res.get(i).get(0);
+            fin[i][1] = res.get(i).get(1);
+        } 
+        return fin;         
     }
 }
