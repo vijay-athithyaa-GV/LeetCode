453ac1b6743bbc392fbdd7cca4802ec97bc875b9
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-07-23 12:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (77.82%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,23 +1,30 @@
-import java.util.*;
 class Solution {
     public void sortColors(int[] nums) {
-       int n = nums.length;
-    //    int res[] = new int[3];
-    //    Arrays.fill(res,0);
-        int cnt0=0,cnt1=0,cnt2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i] == 0) cnt0++;
-            if(nums[i] == 1) cnt1++;
-            if(nums[i] == 2) cnt2++;
+        int n = nums.length;
+        int l = 0;
+        int h = n-1;
+        int mid = 0;
+
+        while(mid <= h){
+            if(nums[mid] == 0){
+                Swap(nums,mid,l);
+                mid++;
+                l++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                Swap(nums,h,mid);
+                h--;
+            }
         }
-        for(int i=0;i<cnt0;i++){
-                nums[i] = 0;
-        }
-        for(int i=cnt0;i<cnt0+cnt1;i++){
-                nums[i] = 1;
-        }
-        for(int i=cnt0+cnt1;i<n;i++){
-                nums[i] = 2;
-        }
+
+    }
+
+    public void Swap(int arr[],int i,int j){
+        int tem = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tem;
     }
 }
