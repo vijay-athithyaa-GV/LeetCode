eb842301ce63ee2ac8729c22e1a9b89c72ecb524
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-08-01 11:55:22 +0530

    Time: 8 ms (61.43%) | Memory: 46.4 MB (13.09%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,31 +1,32 @@
+import java.util.*;
+
 class Solution {
     public String countAndSay(int n) {
-        int val = 1;
-        String res = Integer.toString(val);
-        for(int i=1;i<n;i++){
-            res = count(res);
+        String res = "1";
+        if(n == 1) return res;
+        for(int i=0;i<n-1;i++){
+            res = find(res);
+            System.out.println(res);
         }
         return res;
     }
-    public String count(String s1){
-        int n = s1.length();
-        StringBuilder str = new StringBuilder();
-        int count = 1;
-        char initial = s1.charAt(0);
-        for(int i=1;i<n;i++){
-            char c = s1.charAt(i);
-            if(initial == c) count++;
-            else{
-                str.append(count);
-                str.append(initial);
-                initial = c;
-                count = 1;
+
+    public String find(String res){
+        StringBuilder las = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while(j<res.length()){
+            char c = res.charAt(i);
+            j = i+1;
+            int cnt = 1;
+            while(j<res.length() && c==res.charAt(j)){
+                cnt++;
+                j++;
             }
+            las.append(String.valueOf(cnt));
+            las.append(String.valueOf(c));
+            i = j;
         }
-        str.append(count);
-        str.append(initial);
-        
-        return str.toString();
+        return las.toString();
     }
-   
 }
