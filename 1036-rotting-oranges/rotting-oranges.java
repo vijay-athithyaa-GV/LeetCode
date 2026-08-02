b1156b58504176b400723097c0b27459cc67f3fd
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-08-02 23:50:56 +0530

    Time: 2 ms (89.85%) | Memory: 44.3 MB (24.94%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,67 +1,62 @@
 import java.util.*;
 
 class pair{
-    int row;
-    int col;
-    int val;
-    pair(int row,int col,int val){
-        this.row = row;
-        this.col = col;
-        this.val = val;
+    int i,j,tim;
+    pair(int i,int j,int tim){
+        this.i = i;
+        this.j = j;
+        this.tim = tim;
     }
 }
 
 class Solution {
     public int orangesRotting(int[][] grid) {
+        //0 -> empty
+        //1 -> fresh
+        //2 -> rotten
         int n = grid.length;
         int m = grid[0].length;
         int vis[][] = new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(vis[i],0);
-        }
-        int time = 0;
+        for(int i=0;i<n;i++) Arrays.fill(vis[i],0);
+        int cnt = 0;
         Queue<pair> q = new LinkedList<>();
+        int flg = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
-                    pair p = new pair(i,j,time);
+                    q.add(new pair(i,j,0));
                     vis[i][j] = 1;
-                    q.add(p);
                 }
             }
         }
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+        int tm = 0;
         while(!q.isEmpty()){
-            pair ele = q.poll();
-            int row = ele.row;
-            int col = ele.col;
-            int val = ele.val;
-            time = Math.max(time,val);
-            if(row+1<n && vis[row+1][col] != 1 && grid[row+1][col] == 1){
-                pair p = new pair(row+1,col,val+1);
-                vis[row+1][col] = 1;
-                q.add(p);
-            }
-            if(col+1<m && vis[row][col+1]!=1 && grid[row][col+1] == 1){
-                pair p = new pair(row,col+1,val+1);
-                vis[row][col+1] = 1;
-                q.add(p);
-            }
-            if(row-1>=0 && vis[row-1][col]!=1 && grid[row-1][col] == 1){
-                pair p = new pair(row-1,col,val+1);
-                vis[row-1][col] = 1;
-                q.add(p);
-            }
-            if(col-1>=0 &&  vis[row][col-1]!=1 && grid[row][col-1] == 1){
-                pair p = new pair(row,col-1,val+1);
-                vis[row][col-1] = 1;
-                q.add(p);
-            }
+          //  int size = q.size();
+            pair curr = q.poll();
+            int i = curr.i;
+            int j = curr.j;
+            int t = curr.tim;
+            tm = Math.max(t,tm);
+           
+                for(int k=0;k<4;k++){
+                    int row = i+drow[k];
+                    int col = j+dcol[k];
+                    if(row>=0 && row<n && col>=0 && col<m && vis[row][col]!=1 && grid[row][col]==1){
+                        grid[row][col] = 2;
+                        q.add(new pair(row,col,t+1));
+                        vis[row][col]=1;
+                    }
+                }
         }
         for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(grid[i][j] == 1 && vis[i][j] == 0) return -1;
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]!=1){
+                    return -1;
+                }
             }
         }
-        return time;
+        return tm;
     }
 }
