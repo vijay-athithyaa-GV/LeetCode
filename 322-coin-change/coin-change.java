395a827bbf91e2149bc61612e1bccb98cde32c13
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-08-06 16:24:21 +0530

    Time: 24 ms (32.89%) | Memory: 47.1 MB (20.28%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,9 +3,11 @@ import java.util.*;
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        int res = findCoins(n-1,coins,amount,dp);
+        int dp[][] = new int[n+1][amount+1];
+        for(int i=0;i<=n;i++) Arrays.fill(dp[i],-1);
+        //int res = findCoins(n-1,coins,amount,dp);
+        //return res==Integer.MAX_VALUE?-1:res;
+        int res = tab(coins,amount,dp);
         return res==Integer.MAX_VALUE?-1:res;
     }
 
@@ -25,5 +27,30 @@ class Solution {
             }
         }
         return dp[ind][amt]=Math.min(left,right);
+    }
+
+    public int tab(int arr[],int amount,int dp[][]){
+        int n = arr.length;
+        for(int i=0;i<=amount;i++){
+            if(i%arr[0] == 0)
+                dp[0][i] = i/arr[0];
+            else dp[0][i] = Integer.MAX_VALUE; 
+        }
+        
+
+        for(int ind=1;ind<=n;ind++){
+            for(int amt=0;amt<=amount;amt++){
+                int right = dp[ind-1][amt];
+                int left = Integer.MAX_VALUE;
+                if(arr[ind-1]<=amt){
+                    int res = dp[ind][amt-arr[ind-1]];
+                    if(res != Integer.MAX_VALUE){
+                        left = 1+res;
+                    }
+                }
+                dp[ind][amt]=Math.min(left,right); 
+            }
+        }
+        return dp[n][amount];
     }
 }
