33bc522542491303a1f0abfdbacba00772f6b428
Vijay athithyaa g v <2023503304@student.annauniv.edu>
2026-08-11 15:42:18 +0530

    Time: 6 ms (99.69%) | Memory: 47.8 MB (59.90%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -2,40 +2,29 @@ import java.util.*;
 
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        //ith pile has piles[i] bananas
-        //the guards have gone will come back in h hours.
-        // randome -> eats k bananas from that pile. 
-        // need to return K (min)
-        int MOD = 1_000_000_000;
         int n = piles.length;
-        Arrays.sort(piles);
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            max = Math.max(piles[i],max);
-        }
+        int k = 0;
+        int max = 0;
         int l = 1;
-        int r = max;
-        int ans = Integer.MAX_VALUE;
-        while(l<=r){
-            int mid = (l+r)/2;
-            long tot = totHrs(mid,piles);
-            // if(tot<=h && tot<ans) ans = tot;
-            if(tot<=h){
-                ans = mid;
-                r = mid-1;
+        for(int i=0;i<n;i++) max = Math.max(piles[i],max);
+        while(l<=max){
+            int mid = l+(max-l)/2;
+            if(isPossible(mid,piles,h)){
+                k = mid;
+                max = mid-1;
             }else{
                 l = mid+1;
             }
         }
-        return ans;
+        return k;
     }
 
-    public long totHrs(int mid,int arr[]){
-        int n = arr.length;
-        long sum = 0;
-        for(int i=0;i<n;i++){
-            sum = sum + (arr[i]+mid-1)/mid;
+    public boolean isPossible(int k,int arr[],int h){
+        int temHr = 0;
+        for(int i=0;i<arr.length;i++){
+            temHr += (arr[i]+k-1)/k;
+            if(temHr > h) return false;
         }
-        return sum;
+        return true;
     }
 }
